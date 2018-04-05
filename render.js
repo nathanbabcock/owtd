ec6ade885e2d8a3e6474ff91cdd183e7088a8d0d
nbabcock <nathan.r.babcock@gmail.com>
2018-04-05 09:10:34 -0500

    basic rendering

--- render.js
+++ render.js
@@ -19,33 +19,50 @@ window.addEventListener('resize', () => app.renderer.resize(window.innerWidth - 
 document.body.appendChild(app.view);
 
 var graphics = new PIXI.Graphics();
-
-// set a fill and line style
-graphics.beginFill(0xFF3300);
-graphics.lineStyle(4, 0xffd900, 1);
-
-// draw a shape
-graphics.moveTo(50,50);
-graphics.lineTo(250, 50);
-graphics.lineTo(100, 100);
-graphics.lineTo(50, 50);
-graphics.endFill();
-
-// set a fill and a line style again and draw a rectangle
-graphics.lineStyle(2, 0x0000FF, 1);
-graphics.beginFill(0xFF700B, 1);
-graphics.drawRect(50, 250, 120, 120);
-
-// draw a rounded rectangle
-graphics.lineStyle(2, 0xFF00FF, 1);
-graphics.beginFill(0xFF00BB, 0.25);
-graphics.drawRoundedRect(150, 450, 300, 100, 15);
-graphics.endFill();
-
-// draw a circle, set the lineStyle to zero so the circle doesn't have an outline
-graphics.lineStyle(0);
-graphics.beginFill(0xFFFF0B, 0.5);
-graphics.drawCircle(470, 90,60);
-graphics.endFill();
-
 app.stage.addChild(graphics);
+
+let renderConfig = {
+    grid_size: 25,
+    base_radius: 25,
+    tower_radius: 15,
+};
+
+// Bases
+graphics.beginFill(0xFF3300);
+// graphics.lineStyle(4, 0xffd900, 1);
+map.bases.forEach(base => {
+    graphics.drawCircle(base.x * renderConfig.grid_size, base.y * renderConfig.grid_size, renderConfig.base_radius);
+});
+
+graphics.beginFill(0x0000FF);
+// graphics.lineStyle(4, 0xffd900, 1);
+map.towers.forEach(tower => {
+    graphics.drawCircle(tower.x * renderConfig.grid_size, tower.y * renderConfig.grid_size, renderConfig.tower_radius);
+});
+
+// 
+
+// // draw a shape
+// graphics.moveTo(50,50);
+// graphics.lineTo(250, 50);
+// graphics.lineTo(100, 100);
+// graphics.lineTo(50, 50);
+// graphics.endFill();
+
+// // set a fill and a line style again and draw a rectangle
+// graphics.lineStyle(2, 0x0000FF, 1);
+// graphics.beginFill(0xFF700B, 1);
+// graphics.drawRect(50, 250, 120, 120);
+
+// // draw a rounded rectangle
+// graphics.lineStyle(2, 0xFF00FF, 1);
+// graphics.beginFill(0xFF00BB, 0.25);
+// graphics.drawRoundedRect(150, 450, 300, 100, 15);
+// graphics.endFill();
+
+// // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
+// graphics.lineStyle(0);
+// graphics.beginFill(0xFFFF0B, 0.5);
+// graphics.drawCircle(470, 90,60);
+// graphics.endFill();
+
