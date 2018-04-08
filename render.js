7ba15836b9f3173bdbb29f12df988bc5191e65cc
nbabcock <nathan.r.babcock@gmail.com>
2018-04-08 17:20:08 -0500

    Render lanes

--- render.js
+++ render.js
@@ -36,15 +36,11 @@ map.bases.forEach(base => {
 
 
 // Towers
-
-// graphics.lineStyle(4, 0xffd900, 1);
 map.towers.forEach(tower => {
     graphics.beginFill(0x0000FF);
     graphics.drawCircle(tower.x * renderConfig.grid_size, tower.y * renderConfig.grid_size, renderConfig.tower_radius);
     graphics.endFill();
 });
-
-
 
 // Lanes
 graphics.lineStyle(4, 0x000000);
@@ -59,34 +55,11 @@ graphics.lineStyle(4, 0x000000);
     graphics.lineTo(lane.to.x  * renderConfig.grid_size, lane.to.y * renderConfig.grid_size);
 });
 
-// myGraph.lineStyle(thickness, 0x000000)
-//     .moveTo(0, 0)    
-//     .lineTo(endPoint.x, endPoint.y);
 
-
-// 
-
-// // draw a shape
-// graphics.moveTo(50,50);
-// graphics.lineTo(250, 50);
-// graphics.lineTo(100, 100);
-// graphics.lineTo(50, 50);
-// graphics.endFill();
-
-// // set a fill and a line style again and draw a rectangle
-// graphics.lineStyle(2, 0x0000FF, 1);
-// graphics.beginFill(0xFF700B, 1);
-// graphics.drawRect(50, 250, 120, 120);
-
-// // draw a rounded rectangle
-// graphics.lineStyle(2, 0xFF00FF, 1);
-// graphics.beginFill(0xFF00BB, 0.25);
-// graphics.drawRoundedRect(150, 450, 300, 100, 15);
-// graphics.endFill();
-
-// // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
-// graphics.lineStyle(0);
-// graphics.beginFill(0xFFFF0B, 0.5);
-// graphics.drawCircle(470, 90,60);
-// graphics.endFill();
-
+requestAnimationFrame(update);
+ 
+function update() {
+    console.log("update()");
+ 
+    app.render(app.stage);
+}
